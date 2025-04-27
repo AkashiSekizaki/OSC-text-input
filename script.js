0763203c5dc2f4281282e77c559728e87995a941
AkashiSekizaki <b1021174@fun.ac.jp>
2025-04-27 17:52:34 +0900

    consoleの処理を追加

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.getElementById("displayButton").addEventListener("click", function () {
   // テキストボックス（入力欄）の要素を取得する
   const inputElement = document.getElementById("textInput");
+  console.dir(inputElement);
+  console.log(inputElement.value);
 
   // テキストボックスに入力された値を取り出す
   const inputText = inputElement.value;
